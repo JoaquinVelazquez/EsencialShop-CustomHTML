12c8da1a00e907a22585b8a716469a2440e58b2d
tato <tato.ocampo@live.com.ar>
2022-12-28 17:38:13 -0300

    Favicon

--- static/js/widget-1.js
+++ static/js/widget-1.js
@@ -1,2 +1,5 @@
+let link = document.querySelectorAll("link")[12];
+link.href = 'https://iili.io/hDbnqu.png';
+
 let favicon = document.querySelector("link[rel='shortcut icon']")
 favicon.href = 'https://iili.io/hDbnqu.png';
